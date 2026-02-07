9225a279b4b23dfb3584cf74d0e524f8f77cddd0
meghanabodla <meghanabodla07@gmail.com>
2026-02-07 15:02:07 +0530

    07-02-2026

---
+++ SpringBootDemoApp/src/main/java/com/klu/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.klu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+  @GetMapping("/hello")
+  public String sayHello() {
+    return "this is spring boot demo application";
+  }
+  
+
+}
