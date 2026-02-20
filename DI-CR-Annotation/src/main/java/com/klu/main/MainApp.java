e42f05048b6446e51ba4fdc6d10b9191d20b5360
meghanabodla <meghanabodla07@gmail.com>
2026-02-20 07:44:18 +0530

    24-01-2026

---
+++ DI-CR-Annotation/src/main/java/com/klu/main/MainApp.java
@@ -0,0 +1,16 @@
+package com.klu.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.klu.model.CourseRegistration;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context=new ClassPathXmlApplicationContext("AppConfig.class");
+		CourseRegistration cr=context.getBean("CourseRegistration.class");
+		cr.display();
+	}
+
+}
